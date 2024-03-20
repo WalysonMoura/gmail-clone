203c878a21204e6808482ffa40942ffc145b6a38
{ Walyson } <walysonmoura222@gmail.com>
2024-03-20 22:52:47 +0000

    new

--- src/app/(drawer)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(tabs)/_layout.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Tabs } from 'expo-router';
+import { MaterialIcons } from '@expo/vector-icons';
+import { colors } from '@/styles/colors';
 
 export default function TabLayout() {
- 
   return (
-   
-   <Tabs screenOptions={{
-    
-   }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.orange[500],
+        tabBarInactiveTintColor: colors.gray[400],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          minHeight: 74,
+        },
+        tabBarItemStyle: {
+          paddingBottom: 34,
+          paddingTop: 14,
+        },
+        tabBarShowLabel: false,
+      }}
+      initialRouteName="index">
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="email" size={size} color={color} />,
+        }}
+      />
 
-   </Tabs>
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="chat-bubble" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="meeting"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="videocam" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
