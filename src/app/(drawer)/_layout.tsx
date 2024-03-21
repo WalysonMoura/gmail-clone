d318202b3b12ec9cbc4e0ac69c9b22e94c9946d4
{ Walyson } <walysonmoura222@gmail.com>
2024-03-21 21:09:18 +0000

    new

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,1 +1,19 @@
-export default function DrawerLayout() {}
+import { DrawerContent } from '@react-navigation/drawer';
+import { Drawer } from 'expo-router/drawer';
+import React from 'react';
+
+export default function DrawerLayout() {
+  return (
+    <Drawer
+      defaultStatus="open"
+      drawerContent={(props) => <DrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          width: '75%',
+        },
+      }}>
+      <Drawer.Screen name="(tabs)" options={{ title: '' }} />
+    </Drawer>
+  );
+}
